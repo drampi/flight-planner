963682a068bf1eaa44ba0a746817326238178af3
ddrampi <eriks.ejubs@icloud.com>
2019-03-02 16:02:25 +0200

    persist update 01

--- src/main/java/io/codelex/flightplanner/repository/FlightRecordRepository.java
+++ src/main/java/io/codelex/flightplanner/repository/FlightRecordRepository.java
@@ -10,12 +10,12 @@ import java.util.List;
 public interface FlightRecordRepository extends JpaRepository<FlightRecord, Long> {
 
     @Query("select flight from FlightRecord flight where "
-            + "lower(flight.from.airport) = lower(:from) "
-            + " or lower(flight.to.airport) = lower(:to) "
-            + " or lower(flight.from.city) = lower(:from) "
-            + " or lower(flight.to.city) = lower(:to) "
-            + " or lower(flight.from.country) = lower(:from ) "
-            + " or lower(flight.to.country) = lower(:to)")
+            + "lower(flight.from.airport) like lower(concat('%', :from, '%')) "
+            + " or lower(flight.to.airport) like lower(concat('%', :to, '%'))"
+            + " or lower(flight.from.city) like lower(concat('%', :from, '%'))"
+            + " or lower(flight.to.city) like lower(concat('%', :to, '%'))"
+            + " or lower(flight.from.country) like lower(concat('%', :from, '%'))"
+            + " or lower(flight.to.country) like lower(concat('%', :to, '%'))")
     List<FlightRecord> searchFlights(@Param("from") String from,
                                      @Param("to") String to);
 }
