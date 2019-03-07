4a2e3ad3420a227ada221bb5e5b0b1c016441db0
ddrampi <eriks.ejubs@icloud.com>
2019-03-07 21:22:05 +0200

    Mockito test try 01

--- src/test/java/io/codelex/flightplanner/FlightRecordRepositoryTest.java
+++ src/test/java/io/codelex/flightplanner/FlightRecordRepositoryTest.java
@@ -180,7 +180,7 @@ class FlightRecordRepositoryTest {
     }
 
     @Test
-    void search_from_should_find_flight_by_partial_city() {
+    void search_from_should_find_flight_by_partial_city_first_letters() {
         //given
         AirportRecord RIX = airportRecordRepository.save(new AirportRecord("RIX", "Riga", "LV"));
         AirportRecord DXB = airportRecordRepository.save(new AirportRecord("DXB", "Dubai", "UAE"));
@@ -196,6 +196,336 @@ class FlightRecordRepositoryTest {
 
         //when
         List<FlightRecord> flights = repository.searchFlightsFrom("Ri");
+
+        //then
+        Assertions.assertTrue(flights.contains(flight));
+    }
+
+    @Test
+    void search_from_should_find_flight_by_partial_city_last_letters() {
+        //given
+        AirportRecord RIX = airportRecordRepository.save(new AirportRecord("RIX", "Riga", "LV"));
+        AirportRecord DXB = airportRecordRepository.save(new AirportRecord("DXB", "Dubai", "UAE"));
+
+
+        FlightRecord flight = new FlightRecord();
+        flight.setFrom(RIX);
+        flight.setTo(DXB);
+        flight.setCarrier("Turkish Airlines");
+        flight.setDepartureTime(LocalDate.of(2019, 1, 1).atStartOfDay());
+        flight.setArrivalTime(LocalDate.of(2019, 1, 2).atStartOfDay());
+        repository.save(flight);
+
+        //when
+        List<FlightRecord> flights = repository.searchFlightsFrom("ga");
+
+        //then
+        Assertions.assertTrue(flights.contains(flight));
+    }
+
+    @Test
+    void search_from_should_find_flight_by_partial_city_middle_letters() {
+        //given
+        AirportRecord RIX = airportRecordRepository.save(new AirportRecord("RIX", "Riga", "LV"));
+        AirportRecord DXB = airportRecordRepository.save(new AirportRecord("DXB", "Dubai", "UAE"));
+
+
+        FlightRecord flight = new FlightRecord();
+        flight.setFrom(RIX);
+        flight.setTo(DXB);
+        flight.setCarrier("Turkish Airlines");
+        flight.setDepartureTime(LocalDate.of(2019, 1, 1).atStartOfDay());
+        flight.setArrivalTime(LocalDate.of(2019, 1, 2).atStartOfDay());
+        repository.save(flight);
+
+        //when
+        List<FlightRecord> flights = repository.searchFlightsFrom("ga");
+
+        //then
+        Assertions.assertTrue(flights.contains(flight));
+    }
+
+    @Test
+    void search_from_should_find_flight_by_partial_country_first_letters() {
+        //given
+        AirportRecord RIX = airportRecordRepository.save(new AirportRecord("RIX", "Riga", "Latvia"));
+        AirportRecord DXB = airportRecordRepository.save(new AirportRecord("DXB", "Dubai", "UAE"));
+
+
+        FlightRecord flight = new FlightRecord();
+        flight.setFrom(RIX);
+        flight.setTo(DXB);
+        flight.setCarrier("Turkish Airlines");
+        flight.setDepartureTime(LocalDate.of(2019, 1, 1).atStartOfDay());
+        flight.setArrivalTime(LocalDate.of(2019, 1, 2).atStartOfDay());
+        repository.save(flight);
+
+        //when
+        List<FlightRecord> flights = repository.searchFlightsFrom("Lat");
+
+        //then
+        Assertions.assertTrue(flights.contains(flight));
+    }
+
+    @Test
+    void search_from_should_find_flight_by_partial_country_last_letters() {
+        //given
+        AirportRecord RIX = airportRecordRepository.save(new AirportRecord("RIX", "Riga", "Latvia"));
+        AirportRecord DXB = airportRecordRepository.save(new AirportRecord("DXB", "Dubai", "UAE"));
+
+
+        FlightRecord flight = new FlightRecord();
+        flight.setFrom(RIX);
+        flight.setTo(DXB);
+        flight.setCarrier("Turkish Airlines");
+        flight.setDepartureTime(LocalDate.of(2019, 1, 1).atStartOfDay());
+        flight.setArrivalTime(LocalDate.of(2019, 1, 2).atStartOfDay());
+        repository.save(flight);
+
+        //when
+        List<FlightRecord> flights = repository.searchFlightsFrom("via");
+
+        //then
+        Assertions.assertTrue(flights.contains(flight));
+    }
+
+    @Test
+    void search_from_should_find_flight_by_partial_country_middle_letters() {
+        //given
+        AirportRecord RIX = airportRecordRepository.save(new AirportRecord("RIX", "Riga", "Latvia"));
+        AirportRecord DXB = airportRecordRepository.save(new AirportRecord("DXB", "Dubai", "UAE"));
+
+
+        FlightRecord flight = new FlightRecord();
+        flight.setFrom(RIX);
+        flight.setTo(DXB);
+        flight.setCarrier("Turkish Airlines");
+        flight.setDepartureTime(LocalDate.of(2019, 1, 1).atStartOfDay());
+        flight.setArrivalTime(LocalDate.of(2019, 1, 2).atStartOfDay());
+        repository.save(flight);
+
+        //when
+        List<FlightRecord> flights = repository.searchFlightsFrom("tv");
+
+        //then
+        Assertions.assertTrue(flights.contains(flight));
+    }
+
+    @Test
+    void search_from_should_find_flight_by_partial_carrier_first_letters() {
+        //given
+        AirportRecord RIX = airportRecordRepository.save(new AirportRecord("RIX", "Riga", "Latvia"));
+        AirportRecord DXB = airportRecordRepository.save(new AirportRecord("DXB", "Dubai", "UAE"));
+
+
+        FlightRecord flight = new FlightRecord();
+        flight.setFrom(RIX);
+        flight.setTo(DXB);
+        flight.setCarrier("Turkish Airlines");
+        flight.setDepartureTime(LocalDate.of(2019, 1, 1).atStartOfDay());
+        flight.setArrivalTime(LocalDate.of(2019, 1, 2).atStartOfDay());
+        repository.save(flight);
+
+        //when
+        List<FlightRecord> flights = repository.searchFlightsFrom("RI");
+
+        //then
+        Assertions.assertTrue(flights.contains(flight));
+    }
+
+    @Test
+    void search_from_should_find_flight_by_partial_carrier_last_letters() {
+        //given
+        AirportRecord RIX = airportRecordRepository.save(new AirportRecord("RIX", "Riga", "Latvia"));
+        AirportRecord DXB = airportRecordRepository.save(new AirportRecord("DXB", "Dubai", "UAE"));
+
+
+        FlightRecord flight = new FlightRecord();
+        flight.setFrom(RIX);
+        flight.setTo(DXB);
+        flight.setCarrier("Turkish Airlines");
+        flight.setDepartureTime(LocalDate.of(2019, 1, 1).atStartOfDay());
+        flight.setArrivalTime(LocalDate.of(2019, 1, 2).atStartOfDay());
+        repository.save(flight);
+
+        //when
+        List<FlightRecord> flights = repository.searchFlightsFrom("IX");
+
+        //then
+        Assertions.assertTrue(flights.contains(flight));
+    }
+
+    @Test
+    void search_to_should_find_flight_by_partial_city_first_letters() {
+        //given
+        AirportRecord RIX = airportRecordRepository.save(new AirportRecord("RIX", "Riga", "LV"));
+        AirportRecord DXB = airportRecordRepository.save(new AirportRecord("DXB", "Dubai", "UAE"));
+
+
+        FlightRecord flight = new FlightRecord();
+        flight.setFrom(RIX);
+        flight.setTo(DXB);
+        flight.setCarrier("Turkish Airlines");
+        flight.setDepartureTime(LocalDate.of(2019, 1, 1).atStartOfDay());
+        flight.setArrivalTime(LocalDate.of(2019, 1, 2).atStartOfDay());
+        repository.save(flight);
+
+        //when
+        List<FlightRecord> flights = repository.searchFlightsTo("Dub");
+
+        //then
+        Assertions.assertTrue(flights.contains(flight));
+    }
+
+    @Test
+    void search_to_should_find_flight_by_partial_city_last_letters() {
+        //given
+        AirportRecord RIX = airportRecordRepository.save(new AirportRecord("RIX", "Riga", "LV"));
+        AirportRecord DXB = airportRecordRepository.save(new AirportRecord("DXB", "Dubai", "UAE"));
+
+
+        FlightRecord flight = new FlightRecord();
+        flight.setFrom(RIX);
+        flight.setTo(DXB);
+        flight.setCarrier("Turkish Airlines");
+        flight.setDepartureTime(LocalDate.of(2019, 1, 1).atStartOfDay());
+        flight.setArrivalTime(LocalDate.of(2019, 1, 2).atStartOfDay());
+        repository.save(flight);
+
+        //when
+        List<FlightRecord> flights = repository.searchFlightsTo("uba");
+
+        //then
+        Assertions.assertTrue(flights.contains(flight));
+    }
+
+    @Test
+    void search_to_should_find_flight_by_partial_city_middle_letters() {
+        //given
+        AirportRecord RIX = airportRecordRepository.save(new AirportRecord("RIX", "Riga", "LV"));
+        AirportRecord DXB = airportRecordRepository.save(new AirportRecord("DXB", "Dubai", "UAE"));
+
+
+        FlightRecord flight = new FlightRecord();
+        flight.setFrom(RIX);
+        flight.setTo(DXB);
+        flight.setCarrier("Turkish Airlines");
+        flight.setDepartureTime(LocalDate.of(2019, 1, 1).atStartOfDay());
+        flight.setArrivalTime(LocalDate.of(2019, 1, 2).atStartOfDay());
+        repository.save(flight);
+
+        //when
+        List<FlightRecord> flights = repository.searchFlightsTo("bai");
+
+        //then
+        Assertions.assertTrue(flights.contains(flight));
+    }
+
+    @Test
+    void search_to_should_find_flight_by_partial_country_first_letters() {
+        //given
+        AirportRecord RIX = airportRecordRepository.save(new AirportRecord("RIX", "Riga", "Latvia"));
+        AirportRecord DXB = airportRecordRepository.save(new AirportRecord("DXB", "Dubai", "UnitedArabEmirates"));
+
+
+        FlightRecord flight = new FlightRecord();
+        flight.setFrom(RIX);
+        flight.setTo(DXB);
+        flight.setCarrier("Turkish Airlines");
+        flight.setDepartureTime(LocalDate.of(2019, 1, 1).atStartOfDay());
+        flight.setArrivalTime(LocalDate.of(2019, 1, 2).atStartOfDay());
+        repository.save(flight);
+
+        //when
+        List<FlightRecord> flights = repository.searchFlightsTo("United");
+
+        //then
+        Assertions.assertTrue(flights.contains(flight));
+    }
+
+    @Test
+    void search_to_should_find_flight_by_partial_country_last_letters() {
+        //given
+        AirportRecord RIX = airportRecordRepository.save(new AirportRecord("RIX", "Riga", "Latvia"));
+        AirportRecord DXB = airportRecordRepository.save(new AirportRecord("DXB", "Dubai", "UnitedArabEmirates"));
+
+
+        FlightRecord flight = new FlightRecord();
+        flight.setFrom(RIX);
+        flight.setTo(DXB);
+        flight.setCarrier("Turkish Airlines");
+        flight.setDepartureTime(LocalDate.of(2019, 1, 1).atStartOfDay());
+        flight.setArrivalTime(LocalDate.of(2019, 1, 2).atStartOfDay());
+        repository.save(flight);
+
+        //when
+        List<FlightRecord> flights = repository.searchFlightsTo("Emirates");
+
+        //then
+        Assertions.assertTrue(flights.contains(flight));
+    }
+
+    @Test
+    void search_to_should_find_flight_by_partial_country_middle_letters() {
+        //given
+        AirportRecord RIX = airportRecordRepository.save(new AirportRecord("RIX", "Riga", "Latvia"));
+        AirportRecord DXB = airportRecordRepository.save(new AirportRecord("DXB", "Dubai", "UnitedArabEmirates"));
+
+
+        FlightRecord flight = new FlightRecord();
+        flight.setFrom(RIX);
+        flight.setTo(DXB);
+        flight.setCarrier("Turkish Airlines");
+        flight.setDepartureTime(LocalDate.of(2019, 1, 1).atStartOfDay());
+        flight.setArrivalTime(LocalDate.of(2019, 1, 2).atStartOfDay());
+        repository.save(flight);
+
+        //when
+        List<FlightRecord> flights = repository.searchFlightsTo("Arab");
+
+        //then
+        Assertions.assertTrue(flights.contains(flight));
+    }
+
+    @Test
+    void search_to_should_find_flight_by_partial_carrier_first_letters() {
+        //given
+        AirportRecord RIX = airportRecordRepository.save(new AirportRecord("RIX", "Riga", "Latvia"));
+        AirportRecord DXB = airportRecordRepository.save(new AirportRecord("DXB", "Dubai", "UAE"));
+
+
+        FlightRecord flight = new FlightRecord();
+        flight.setFrom(RIX);
+        flight.setTo(DXB);
+        flight.setCarrier("Turkish Airlines");
+        flight.setDepartureTime(LocalDate.of(2019, 1, 1).atStartOfDay());
+        flight.setArrivalTime(LocalDate.of(2019, 1, 2).atStartOfDay());
+        repository.save(flight);
+
+        //when
+        List<FlightRecord> flights = repository.searchFlightsTo("DX");
+
+        //then
+        Assertions.assertTrue(flights.contains(flight));
+    }
+
+    @Test
+    void search_to_should_find_flight_by_partial_carrier_last_letters() {
+        //given
+        AirportRecord RIX = airportRecordRepository.save(new AirportRecord("RIX", "Riga", "Latvia"));
+        AirportRecord DXB = airportRecordRepository.save(new AirportRecord("DXB", "Dubai", "UAE"));
+
+
+        FlightRecord flight = new FlightRecord();
+        flight.setFrom(RIX);
+        flight.setTo(DXB);
+        flight.setCarrier("Turkish Airlines");
+        flight.setDepartureTime(LocalDate.of(2019, 1, 1).atStartOfDay());
+        flight.setArrivalTime(LocalDate.of(2019, 1, 2).atStartOfDay());
+        repository.save(flight);
+
+        //when
+        List<FlightRecord> flights = repository.searchFlightsTo("XB");
 
         //then
         Assertions.assertTrue(flights.contains(flight));
---
+++ src/test/java/io/codelex/flightplanner/repository/RepositoryFlightServiceTest.java
@@ -0,0 +1,93 @@
+package io.codelex.flightplanner.repository;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import io.codelex.flightplanner.api.AddTripRequest;
+import io.codelex.flightplanner.api.Airport;
+import io.codelex.flightplanner.api.FindTripRequest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(RepositoryFlightService.class)
+public class RepositoryFlightServiceTest {
+
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    static {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addDeserializer(
+                LocalDateTime.class,
+                new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))
+        );
+        javaTimeModule.addDeserializer(
+                LocalDate.class,
+                new LocalDateDeserializer(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+        );
+
+        javaTimeModule.addSerializer(
+                LocalDateTime.class,
+                new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))
+        );
+        javaTimeModule.addSerializer(
+                LocalDate.class,
+                new LocalDateSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd"))
+        );
+
+        builder.modules(javaTimeModule);
+        builder.featuresToDisable(WRITE_DATES_AS_TIMESTAMPS);
+
+        MAPPER.registerModule(javaTimeModule);
+    }
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private RepositoryFlightService service;
+
+    @Test
+    public void should_be_able_to_add_trip_and_return_status_200_OK() throws Exception {
+        //given
+        AddTripRequest request = new AddTripRequest(
+                new Airport("Latvia", "Riga", "RIX"),
+                new Airport("UAE", "Dubai", "DXI"),
+                "Turkish Airlines",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusDays(1)
+        );
+        String json = MAPPER.writeValueAsString(request);
+
+        //expect
+        mockMvc.perform(
+                put("/internal-api/flights")
+                        .content(json)
+                        .contentType(APPLICATION_JSON)
+                        .accept(APPLICATION_JSON))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isNotFound()
+                );
+    }
+}
